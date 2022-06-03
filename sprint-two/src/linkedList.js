f1c8b01886df9e89a5726936cc4c143caac32f72
chrisxchoi <chris@choi.is>
2022-06-03 15:14:56 -0700

    Add linkedList implementation

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,39 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //first create a node
+    var node = Node(value);
+    //check if head is undefined, if it dose, make the first node as our head
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      //if it doesn't, move our pointer to new node
+      this.tail.next = node;
+      //add the node to the tail
+      this.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    if (this.head) {
+      var shifted = this.head.value;
+      this.head = this.head.next;
+      return shifted;
+    } else {
+      return null;
+    }
   };
 
   list.contains = function(target) {
+    var current = this.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   };
 
   return list;
